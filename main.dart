d63d081eda9e78d62f9f46235a6d40d82fe98d57
askvart <tur1amant@gmail.com>
2022-12-10 18:01:31 +0300

    it`s map, list, set

--- main.dart
+++ main.dart
@@ -35,6 +35,8 @@ void main() {
   print("summa:$summa");
   print(add(12, 13));
   hello("Привет функция(процедура)");
+
+  // Списки
   List<int> lst1 = [11, 12, 13, 14, 15, 88];
   for (var i in lst1){
     print("li:$i");
@@ -67,9 +69,99 @@ void main() {
   print(lst5);
   var intList = [1,7,9,0,3, 12, 24];
   var stringList = ['hello', 'ome', 'row'];
+  print(stringList);
   var intList2 = [
     for (var i in intList) if (i % 2 == 0)i
       ];
 
   print(intList2);
+  // Копирование и глубокое копирование списков
+  List<int> x_list1 = [1,5,9,10];
+  var y_list = x_list1;
+  y_list[0] = 777;
+
+  var z_list = List.from(x_list1);
+  z_list.add(333);
+  print("$x_list1, '-', $y_list, '-', $z_list");
+
+  var r_list = [...z_list];
+  r_list[0] = 999;
+  print("$r_list, $x_list1");
+
+  var s_list = []..addAll(r_list);
+  s_list.add(911);
+  print(s_list);
+
+  //Глубокое копирование
+  List<List> xxx_list = [[1,2,3,4,5],[0]];
+  var myZ = xxx_list;
+  myZ.add([[1],[555]]);
+  var myNewList1 = List.from(xxx_list);
+  myNewList1[1].addAll([22,22,22]);
+  print("$xxx_list, '-',$myNewList1");
+
+
+
+  // Множества
+  var mySet1 = {1,3,5,7,9,0};
+  print(mySet1);
+  Set<String> myset2 = {'w', 'd', 'fg'};
+  print(myset2);
+
+  List<int> nList1 = [1,1,1,3,3,5,6,77,77,8,98,99,0];
+  var newList2 = Set<int>.from(nList1).toList();
+  print(newList2);
+  mySet1.addAll([1,3,5,100]);
+  print(mySet1);
+
+  var newSet = <int>{for (var i in nList1) if (i % 2 == 0) i};
+  print(newSet);
+  print("Объединение: ${mySet1.union(newSet)}");
+  print("Пересечение: ${mySet1.intersection(newSet)}");
+  print("Разница: ${mySet1.difference(newSet)}");
+
+
+  // Копирование и глубокое копирование множеств
+  var mySet11 = {1,2,3,8,9};
+  var my2set = mySet11;
+  var my3set = Set.from(mySet11);
+
+  print("$mySet11");
+  print("$my2set");
+  print("$my3set");
+
+  // Map (словари карты)
+  Map<int, String> dict1 = {1:'Killer', 2:'Queen'};
+  print("$dict1, '-', ${dict1.length}");
+  print("${dict1.keys.toSet()}, ${dict1.values.toList()}");
+
+  // Runes
+
+  var muRuna = '\u041F';
+  print(muRuna);
+  Runes myR = Runes('Привет!');
+  print(myR);
+
+  var mySymbol = #myApi;
+  print(mySymbol);
+  Symbol myS = #ghjk;
+  print(myS);
+
+  // Dynamic vars
+  dynamic myValue = 'qwerty';
+  myValue = 1;
+  myValue = 0.765432;
+  myValue = #lkjhgfd;
+  myValue = [1,2,'w'];
+  print(myValue);
+
+  List<dynamic> dynaLst1 = [1,3,'wer', 8, 9,'qw'];
+  print(dynaLst1);
+  dynaLst1.add("value");
+  print(dynaLst1);
+
+  Map<dynamic, dynamic> myDynaMapa = {1:'hELLO', 'WORLD': 'TRUE', 'KISS': 22};
+  print(myDynaMapa);
+
+
 }
